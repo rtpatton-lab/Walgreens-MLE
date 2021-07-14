bc50a9394ecac2e963e4bfc6d5ca5936ed0769c6
rtpatton-lab <66214517+rtpatton-lab@users.noreply.github.com>
2021-07-14 12:30:42 -0500

    updating count_some

--- count_some.py
+++ count_some.py
@@ -1,10 +1,30 @@
-class Solution:
-    def count_some(starting_number: int, ending_number: int, k: int) -> int:
-        count = 0
-        for i in range(starting_number, ending_number):
-            if (i % k == 0):
-                count += 1
+def count_some(starting_num, ending_num, k):
+    lst = []
+    if (starting_num == ending_num):
+        lst.append(starting_num)
+    else:
+        while (starting_num < ending_num + 1):
+            lst.append(starting_num)
+            starting_num += 1
 
-        return count
+    counter = 0
+    n = len(lst)
+    for i in range(0, n, 1):
+        if (lst[i] % k == 0):
+            counter += 1
+
+    return counter
+
+if __name__ == '__main__':
+    starting_num = 0
+    ending_num = 10
+    k = 5
+
+    print(count_some(starting_num, ending_num, k))
+
+
+
+
+
 
     
